76414eb51e942fbe5ad2bf5f73cd99706ec71053
Nate Wessel <nate.wessel@toronto.ca>
2026-02-04 20:23:37 -0500

    exclude null geoms and plough damage

--- inputs/requests_filtered.sql
+++ inputs/requests_filtered.sql
@@ -5,8 +5,11 @@ FROM nwessel.requests_raw
 WHERE
     -- specified that these be removed as they were being handled already
     -- or managed through a separate process
-    failurecode != 'BUS STOP'
+    geom IS NOT NULL
+    AND failurecode != 'BUS STOP'
+    AND description !~* 'plough damage'
     AND NOT (
         failurecode = 'SIDEWALK'
         AND ownergroup != 'TSFITEY'
     );
+    
