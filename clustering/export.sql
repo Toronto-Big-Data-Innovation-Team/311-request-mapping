684b56039d9733f16ac50020a4e64e275ded4481
Nate Wessel <nate.wessel@toronto.ca>
2026-02-04 20:57:14 -0500

    modify export query, add comment

--- clustering/export.sql
+++ clustering/export.sql
@@ -1,7 +1,10 @@
 -- dump the data with cluster IDs
+-- Easiest to just sort the spreadsheet they gave us by wonum
+-- and append the group_names directly to that as a column
+-- be sure to verify that wonums align
 SELECT
-    requests.*,
-    cluster_id,
+    wonum,
     group_id
 FROM nwessel.requests_raw AS requests
-JOIN nwessel.kmeans_groups USING (wonum)
+LEFT JOIN nwessel.kmeans_grouped_requests USING (wonum)
+ORDER BY wonum;
