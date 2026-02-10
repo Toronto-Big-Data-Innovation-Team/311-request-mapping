a797436ac61e2a6f1313e096c6334a1e6ebf116e
Aakash Harpalani <aakash.harpalani@toronto.ca>
2026-02-09 21:57:56 -0500

    migrate tables and views to `data_analysis` schema

--- inputs/requests_raw.sql
+++ inputs/requests_raw.sql
@@ -4,25 +4,35 @@ Import from CSV with psql
 but first make sure all the fields are the same and in the same order
 */
 
-CREATE TABLE nwessel.requests_raw (
+CREATE TABLE data_analysis.m311_requests_raw (
     wonum numeric,
     description text,
     location text,
     failurecode text,
     problemcode text,
+    status text,
+    supervisor text,
     ownergroup text,
+    worktype text,
+    wopriority text,
     reportdate text,
     targstartdate text,
     siteid text,
+    statusdate text,
+    assignedownergroup text,
     -- provided as EPSG:102113
     latitudey numeric,
-    longitudex numeric
+    longitudex numeric,
+    cotward text
 );
+
+ALTER TABLE data_analysis.m311_requests_raw OWNER TO analysis_admins; -- would like this to be its own distinct role
+
 
 -- run after to make geometries
 
-ALTER TABLE nwessel.requests_raw ADD COLUMN geom geometry(POINT, 4326);
-UPDATE nwessel.requests_raw SET geom = ST_Transform(
+ALTER TABLE data_analysis.m311_requests_raw ADD COLUMN geom geometry(POINT, 4326);
+UPDATE data_analysis.m311_requests_raw SET geom = ST_Transform(
     ST_SetSRID(ST_MakePoint(longitudex, latitudey), 102113),
     4326
 );
