394b8491c0b7e675c720fb8df573529d416f9384
Aakash Harpalani <aakashharpalani@gmail.com>
2026-02-19 13:26:09 -0500

    Move tables/views from personal schema to data_analysis (#4)
    * migrate tables and views to `data_analysis` schema
    
    * more edits to reflect new location
    
    * Change table/view permissions and update filenames
    
    * Update layer references in `cluster_overview.qgs`

---
+++ clustering/m311_kmeans_group_boundaries.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW data_analysis.m311_kmeans_group_boundaries AS
+
+SELECT
+    row_number() OVER () AS uid,
+    group_id,
+    ownergroup,
+    cluster_id,
+    ST_ConvexHull(ST_Collect(geom)) AS bound_geom,
+    COUNT(*) AS n
+FROM data_analysis.m311_kmeans_grouped_requests
+GROUP BY
+    group_id,
+    ownergroup,
+    cluster_id
+HAVING COUNT(*) > 2;
+
+ALTER VIEW data_analysis.m311_kmeans_group_boundaries OWNER TO team_ds;
+GRANT SELECT ON data_analysis.m311_kmeans_group_boundaries TO bdit_humans;
